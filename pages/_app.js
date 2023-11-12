8c7b5db7d94f30b8eae290c9eeb3b5b687e2dd19
Eneko96 <imr.aem2526@gmail.com>
2023-11-12 18:22:36 +0000

    dark

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/global.css";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "../components/theme-provider";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
